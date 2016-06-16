ffdad777b97f1c7ddf7edc6718d259838cc237e1
“FionaSelanno” <fcs_fiona@hotmail.com>
2016-06-16 11:51:35 +0200

    Require log in to post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def handle
+    self.email.split('@')[0]
+  end
+
 end
