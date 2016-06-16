be6b5ef5bd65ae22b37916fcf56cb44f4482ed17
“FionaSelanno” <fcs_fiona@hotmail.com>
2016-06-16 11:57:09 +0200

    users posts page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   get 'posts/index'
+  get 'user_posts/:user_id' => 'posts#user', as: :user_posts
 
 
   # The priority is based upon order of creation: first created -> highest priority.
