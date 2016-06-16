26154274c638940befd0ef480e176523a8ba2d9d
“FionaSelanno” <fcs_fiona@hotmail.com>
2016-06-16 12:19:59 +0200

    Authorize controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.order( created_at: :desc )
+    authorize! :read, @posts
   end
 
 
@@ -9,6 +10,7 @@ class PostsController < ApplicationController
 
      @post = Post.new( content: post_params[:content] )
      @post.user = current_user
+     authorize! :create, @post
 
      if @post.save
         redirect_to posts_path
@@ -19,9 +21,12 @@ class PostsController < ApplicationController
 
   def user
       @user = User.find( params[:user_id] )
+      authorize! :read, @user
 
       @posts = Post.where( user: @user ).order( created_at: :desc )
+      authorize! :read, @posts
 
       @likes = @user.likes.joins( :post ).order( "posts.created_at DESC" )
+      authorize! :read, @likes
    end
 end
